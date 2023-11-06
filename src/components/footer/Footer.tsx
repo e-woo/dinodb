fbaf63fd542d34cf3f892ab8357b12599e913208
Joseph Tandyo <josephtandyo@gmail.com>
2023-11-06 01:08:09 -0700

    Footer style added

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import './style.css'
+import React from "react";
+import "./style.css";
 const Footer = () => {
-    return (
-        <footer>
-            <span>Footer</span>
-            <span>Footer2</span>
-        </footer>
-    );
-}
+  return (
+    <footer className="footer">
+      <div className="containerF">
+        <div className="row">
+          <div className="footerCol">
+            <h4>Opportunities</h4>
+            <ul>
+              <li>
+                <a href="#">Clubs</a>
+              </li>
+              <li>
+                <a href="#">Volunteer</a>
+              </li>
+              <li>
+                <a href="#">Programs</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footerCol">
+            <h4>Announcements</h4>
+            <ul>
+              <li>
+                <a href="#">News</a>
+              </li>
+              <li>
+                <a href="#">Events</a>
+              </li>
+              <li>
+                <a href="#">Calendar</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
