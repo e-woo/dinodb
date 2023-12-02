4221b4e97b6615d42335575b665c7a8ecc3dddb9
ethanw <ethansfwoo@gmail.com>
2023-12-02 14:45:14 -0700

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+  extend: {
+    fontFamily: {
+      'sans': ['"Nunito"', ...defaultTheme.fontFamily.sans]
+    }
+  },
+};
+export const plugins = [];
+
