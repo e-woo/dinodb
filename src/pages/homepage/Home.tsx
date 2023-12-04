3733173ae102466facbde59a26433b1ef9546440
ethanw <ethansfwoo@gmail.com>
2023-12-03 22:28:27 -0700

    homepage done and responsive

--- src/pages/homepage/Home.tsx
+++ src/pages/homepage/Home.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "./style.css";
 import logo from "./uofc-logo.png";
 import axios from "axios";
 
@@ -45,51 +44,37 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="home">
-      <div className="landing-page">
-        <div className="text">
-          <h1>
-            Welcome to <br></br> Dino DB
+    <div className=''>
+      <div className='flex flex-col md:flex-row py-24 px-16 md:gap-48 justify-center align-center bg-gradient-to-b from-red-600 via-red-500 to-red-600'>
+        <div className='flex flex-col justify-center items-center lg:items-start text-center lg:text-left'>
+          <h1 className='text-6xl lg:text-8xl font-extrabold text-[#f5f7f8] max-w-fit mb-4'>
+            Welcome to
           </h1>
-          <h3>
+          <h1 className='text-6xl lg:text-8xl font-extrabold text-[#f5f7f8] max-w-fit'>
+            Dino DB
+          </h1>
+          <h3 className='text-lg text-[#f5f7f8] mb-5 mt-2'>
             Embark on Your UCalgary Adventure <br></br>
             Connect with Your Future Community <br></br>
             Explore All Extracurriculars,{" "}
-            <span className="gold-text">All in One Spot</span>
+            <span className='text-[#ffcd00]'>All in One Spot</span>
           </h3>
-          <Link className="link" to={`/search`}>
-            <button className="exploreButton">Explore Now</button>
+          <Link to={`/search`}>
+            <button className='w-max py-3 px-6 rounded-3xl text-base font-bold border-4 border-red-600 bg-[#f5f7f8] text-red-500 transition-[.3s] ease-in-out hover:bg-[#ffe57b]'>
+              Explore Now
+            </button>
           </Link>
         </div>
-        <div className="big-logo">
+        <div className="h-1/2 w-auto hidden lg:block">
           <img src={logo} alt="UofC Logo" />
         </div>
       </div>
-      <div className="benefits">
-        <div className="benefitContainer">
-          <h1 className="benefitH1">Community</h1>
-          <p className="benefitP">
-            Participation in activities often leads to the formation of lasting
-            friendships, creating a support system that extends beyond the
-            college years.
-          </p>
-        </div>
-        <div className="benefitContainer">
-          <h1 className="benefitH1">Network</h1>
-          <p className="benefitP">
-            {" "}
-            Being active provides opportunities to network with professors,
-            professionals, and fellow students, potentially leading to
-            internships or job opportunities.
-          </p>
-        </div>
-        <div className="benefitContainer">
-          <h1 className="benefitH1">Resume</h1>{" "}
-          <p className="benefitP">
-            Active involvement on campus showcases leadership abilities on a
-            resume, making students more attractive to potential employers.
-          </p>
-        </div>
+      <div className='mx-8 flex flex-col lg:flex-row lg:gap-24 items-center justify-center'>
+        {benefits.map((benefit) => 
+        <div className='bg-red-500 max-w-[500px] text-[#f5f7f8] mt-10 p-7 min-h-[200px] flex flex-col align-center justify-center rounded-xl'>
+          <h1 className='text-[2.5rem] font-black mb-5 text-center'>{benefit.header}</h1>
+          <p className='text-center'>{benefit.body}</p>
+        </div>)}
       </div>
       <ClubsSlider
         header="Discover Clubs"
@@ -98,20 +83,20 @@ const Home = () => {
         type="club"
         posts={clubs}
       />
-      <div className="createClub">
-        <h1 className="benefitH1">
+      <div className='bg-red-500 flex flex-col items-center justify-center text-[#f5f7f8] p-12 min-h-[400px]'>
+        <h1 className='text-[2.5rem] font-black mb-5 text-center'>
           Apply as an Executive and Start Your Own{" "}
-          <span className="gold-text">Club</span> Today!
+          <span className='text-[#ffcd00]'>Club</span> Today!
         </h1>
-        <p className="centerP">
+        <p className='text-xl text-center'>
           Ready to lead? Join as an executive in an existing club or ignite your
           own journey by founding a brand new one.
-          <br></br>Your vision, your rules—be the trailblazer and leave a
+          <br/>Your vision, your rules—be the trailblazer and leave a
           lasting impact on the university community.
-          <br></br>
+          <br/>
         </p>
-        <Link className="link" to={`/register`}>
-          <button className="postsButton findButton">
+        <Link to={`/register`}>
+          <button className='w-max my-4 py-3 px-6 rounded-3xl text-base font-bold border-4 border-red-600 bg-[#f5f7f8] text-red-500 transition-[.3s] ease-in-out hover:bg-[#ffe57b]'>
             Your leadership story begins here!
           </button>
         </Link>
@@ -128,31 +113,31 @@ const Home = () => {
         type="program"
         posts={programs}
       />
-      <div className="createClub">
-        <h1 className="benefitH1">
+      <div className='bg-red-500 flex flex-col items-center justify-center text-[#f5f7f8] p-12 min-h-[400px]'>
+        <h1 className='text-[2.5rem] font-black mb-5 text-center'>
           Elevate Your Club Experience -{" "}
-          <span className="gold-text">Join Us Today!</span>
+          <span className='text-[#ffcd00]'>Join Us Today!</span>
         </h1>
-        <div className="benefitsList">
-          <div className="benefitSection">
-            <h2 className="benefitTitle">Club Executives</h2>
-            <ul className="benefitItems">
+        <div>
+          <div className='mb-5 text-center'>
+            <h2 className='text-[1.8rem] font-bold text-[#f5f7f8] mb-2'>Club Executives</h2>
+            <ul className='flex flex-col gap-4 text-[#f5f7f8] font-medium'>
               <li>Announce with impact, manage with ease.</li>
               <li>Seamless event coordination at your fingertips.</li>
               <li>Engage and inspire your team, leading by example.</li>
             </ul>
           </div>
-          <div className="benefitSection">
-            <h2 className="benefitTitle">Club Members</h2>
-            <ul className="benefitItems">
+          <div className='mb-5 text-center'>
+            <h2 className='text-[1.8rem] font-bold text-[#f5f7f8] mb-2'>Club Members</h2>
+            <ul className='flex flex-col gap-4 text-[#f5f7f8] font-medium'>
               <li>Simplify your club management and tracking.</li>
               <li>Stay updated with tailored club notifications.</li>
               <li>Organize your club events and calendars effortlessly.</li>
             </ul>
           </div>
         </div>
-        <Link className="link" to={`/login`}>
-          <button className="postsButton findButton">Sign In Now</button>
+        <Link to={`/login`}>
+          <button className='w-max my-4 py-3 px-6 rounded-3xl text-base font-bold border-4 border-red-600 bg-[#f5f7f8] text-red-500 transition-[.3s] ease-in-out hover:bg-[#ffe57b]'>Sign In Now</button>
         </Link>
       </div>
       <ClubsSlider
@@ -177,24 +162,24 @@ const ClubsSlider = ({
   posts: Array<{ Activity_ID: number; Name: string; Description: string; Img_file_path: string }>;
 }) => {
   return (
-    <div className="postsContainer">
-      <div className="postTop">
-        <div className="postsHeader"> {header}</div>
-        <div className="postsDesc">{desc}</div>
+    <div className='flex flex-col px-24 py-12 no-underline items-center'>
+      <div className='flex flex-col justify-center items-center w-full h-64 pb-2 text-[#333]'>
+        <div className='flex text-4xl font-extrabold text-center'>{header}</div>
+        <div className='flex text-lg text-center my-5'>{desc}</div>
       </div>
-      <div className="posts">
+      <div className='grid md:grid-cols-2 xl:grid-cols-4 gap-16 justify-center align-center'>
         {posts.map((post) => (
-          <div className="post" key={post.Activity_ID}>
-            <div className="postImg">
-              <img src={post.Img_file_path} alt="" />
+          <div className='flex-1 h-[420px] p-5 rounded-md border-red-500 border-4 overflow-hidden flex flex-col justify-between transition-[.3s] ease-in-out' key={post.Activity_ID}>
+            <div className='max-h-[200px] relative overflow-hidden'>
+              <img src={post.Img_file_path} alt="" className='w-full max-h-full object-cover' />
             </div>
-            <div className="postContent">
-              <Link className="link" to={`/${type}/${post.Activity_ID}`}>
-                <h1 className="postH1">{post.Name}</h1>
+            <div className='flex-[2] flex flex-col justify-between'>
+              <Link to={`/${type}/${post.Activity_ID}`}>
+                <h1 className='text-red-500 text-lg whitespace-normal overflow-hidden text-ellipsis line-clamp-1'>{post.Name}</h1>
               </Link>
-              <p className="postP">{post.Description}</p>
-              <Link className="link" to={`/${type}/${post.Activity_ID}`}>
-                <button className="postsButton">Learn More</button>
+              <p className='text-sm whitespace-normal overflow-hidden text-ellipsis line-clamp-3'>{post.Description}</p>
+              <Link to={`/${type}/${post.Activity_ID}`}>
+                <button className='w-max my-4 py-3 px-6 rounded-lg text-base font-bold border-2 border-red-600 bg-[#f5f7f8] text-red-500 transition-[.3s] ease-in-out hover:bg-[#ffe57b]'>Learn More</button>
               </Link>
             </div>
           </div>
@@ -203,5 +188,20 @@ const ClubsSlider = ({
     </div>
   );
 };
+
+const benefits = [
+  {
+    header: 'Community',
+    body: 'Participation in activities often leads to the formation of lasting friendships, creating a support system that extends beyond the college years.'
+  },
+  {
+    header: 'Network',
+    body: 'Being active provides opportunities to network with professors, professionals, and fellow students, potentially leading to internships or job opportunities.'
+  },
+  {
+    header: 'Resume',
+    body: 'Active involvement on campus showcases leadership abilities on a resume, making students more attractive to potential employers.'
+  }
+]
 
 export default Home;
