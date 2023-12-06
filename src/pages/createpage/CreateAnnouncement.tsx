cbb6949a93cb3e821a02eaee98ba15250eaf5c2c
ewoo <ethansfwoo@gmail.com>
2023-12-06 09:36:54 -0700

    createannouncement page done and responsive

--- src/pages/createpage/CreateAnnouncement.tsx
+++ src/pages/createpage/CreateAnnouncement.tsx
@@ -60,18 +60,18 @@ const CreateAnnouncement = () => {
     };
 
     return (
-    <div className='create'>
+    <div className='min-h-[60vh]'>
         {exec ? <>
-        <h1 className="bigHeader">Post Announcement!</h1>
+        <h1 className='pt-16 pb-8 text-4xl md:text-5xl lg:text-6xl font-extrabold text-[#333] text-center'>Post Announcement!</h1>
             <form onSubmit={handleSubmit} method='post'>
-                <div className='createBody'>
-                    <input type='text' placeholder='Title' id='title' required/>
-                    <textarea placeholder='Body...' id='body' rows={6} required/>
-                    <button type='submit'>Post</button>
-                    {duplicateWarning ? <h3 className='warningText'>This title already exists! Please use a different one.</h3> : <></>}
+                <div className='flex flex-col items-center gap-6'>
+                    <input type='text' placeholder='Title' id='title' className='w-56 md:w-72 lg:w-96 border-2 border-[#c6c6c6] rounded-[40px] px-4 py-2 text-sm' required/>
+                    <textarea placeholder='Body...' id='body' rows={6} className='w-56 md:w-72 lg:w-96 border-2 border-[#c6c6c6] rounded-2xl px-4 py-2 text-sm resize-none' required/>
+                    <button type='submit' className='w-56 md:w-72 lg:w-96 border-2 border-red-500 bg-white rounded-xl py-2 justify-center text-lg font-semibold text-red-500 transition-[.3s] ease-in-out hover:bg-red-500 hover:text-white'>Post</button>
+                    {duplicateWarning ? <h3 className='text-red-500 font-semibold'>This title already exists! Please use a different one.</h3> : <></>}
                 </div>
             </form>
-        </> : <h1 className="bigHeader">You do not have permission to access this page!</h1>
+        </> : <h1 className='pt-16 pb-8 text-4xl md:text-5xl lg:text-6xl font-extrabold text-[#333] text-center'>You do not have permission to access this page!</h1>
         }
     </div>
   )
